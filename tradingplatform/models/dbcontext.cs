c045c8e5ff83310d2bca2ece5d359792abc34c54
LIXING <xingliself@gmail.com>
2017-08-08 00:14:34 +0800

    Update DBContext.cs

--- tradingplatform/models/dbcontext.cs
+++ tradingplatform/models/dbcontext.cs
@@ -8,23 +8,32 @@ namespace TradingPlatform.Models
 {
     public class DBContext : DbContext
     {
-        public DBContext()    // : base("name=SQLServerConnectionString")
+        public DBContext() : base("name=DBContext")
         {
             Database.SetInitializer(new DropCreateDatabaseIfModelChanges<DbContext>());
         }
 
-        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<Execution> Stocks { get; set; }
         public DbSet<Trader> Traders { get; set; }
         public DbSet<Trade> Trades { get; set; }
-        public DbSet<MarketData> MarketDatas { get; set; }
+        public DbSet<Order> Orders { get; set; }
         public DbSet<Strategy> Strategies { get; set; }
+        public DbSet<OrderBook> OrderBooks { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<MarketData>().HasRequired(s => s.Stock).WithMany(s => s.MarketDatas);
-            modelBuilder.Entity<Trade>().HasRequired(s => s.Stock).WithMany(s => s.Trades);
-            modelBuilder.Entity<Trade>().HasRequired(s => s.Stragety).WithMany(s => s.Trades);
-            modelBuilder.Entity<Trade>().HasRequired(s => s.Trader).WithMany(s => s.Trades);
+            modelBuilder.Entity<Order>().HasRequired(s => s.Trader).WithMany(s => s.Orders);
+            modelBuilder.Entity<Order>().HasRequired(s => s.Strategy).WithMany(s => s.Orders);
+            modelBuilder.Entity<Order>().HasRequired(s => s.OrderBook).WithMany(s => s.Orders);
+            modelBuilder.Entity<Trade>().HasRequired(s => s.Execution).WithMany(s => s.Trades);
+
+            modelBuilder.Entity<Execution>().HasRequired(s => s.Order).WithOptional(s => s.Execution);
+            modelBuilder.Entity<Trader>().HasMany(s => s.OrderBooks).WithMany(s => s.Traders).Map
+                (s => {
+                    s.MapLeftKey("TraderId");
+                    s.MapRightKey("OrderBookId");
+                    s.ToTable("MonitoredStock");
+                });
 
             base.OnModelCreating(modelBuilder);
         }
