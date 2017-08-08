eabfb7e0ac08e0c5f397ce3afd852cbf69e8034f
LIXING <xingliself@gmail.com>
2017-08-08 16:15:18 +0800

    Combine the frontend and the backend data.

---
+++ TradingPlatform.Tests/Service/OrderBookServiceTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TradingPlatform.Service;
+using TradingPlatform.Service.Impl;
+using TradingPlatform.Models;
+
+namespace TradingPlatform.Tests.Service
+{
+    [TestClass]
+    public class OrderBookServiceTest
+    {
+        private IOrderBookService service;
+
+        [TestInitialize]
+        public void init()
+        {
+            service = new OrderBookService();
+        }
+
+        [TestMethod]
+        public void TestAddOrderBook()
+        {
+            OrderBook orderBook = new OrderBook { IsBit = false, Orders = null, Price = 23, Quantity = 35, Symbol = "Apple", Traders = null };
+            service.AddOrderBooK(orderBook);
+        }
+    }
+}
--- TradingPlatform/Controllers/HomeController.cs
+++ TradingPlatform/Controllers/HomeController.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TradingPlatform.Models;
+using TradingPlatform.Service;
+using TradingPlatform.Service.Impl;
 
 namespace TradingPlatform.Controllers
 {
     public class HomeController : Controller
     {
+        private IOrderBookService service = new OrderBookService();
+
         public ActionResult Index()
         {
+            List<OrderBook> orderBooks = service.FindAllOrderBooks();
+            ViewData["orderBooks"] = orderBooks;
+
             return View();
         }
 
